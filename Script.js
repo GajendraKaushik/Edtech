b24591769d727190c67a2a140b70b47e585bf2d4
GajendraKaushik <gajendrakaushik128@gmail.com>
2024-01-16 20:13:58 +0530

    Added product add and remove fuctionality

--- Script.js
+++ Script.js
@@ -1,14 +1,28 @@
 
 let myCart = []
-
+function add(n){
+    n += 1
+    document.getElementById('quantity').innerHTML = n;
+}
+function minus(n){
+    if (n<=0){
+        alert("Please select valid number of item")
+    } 
+    n -= 1
+    document.getElementById('quantity').innerHTML = n;
+}
 function buy(){
-    let item_des = document.getElementById("card-des").innerText
-    let pri = document.getElementById("price").innerText
+    let item_des = document.getElementById("card-des").innerText;
+    let pri = document.getElementById("price").innerText;
+    let count = Number(document.getElementById("quantity").innerText);
+    pri = Number(pri.split(" ")[0].slice(1))
     itme = {
          title : item_des,
-         price: pri
+         price: [pri, count * pri],
+         total: count * pri, 
+         quantity:count
     }
-      myCart.push(itme)
+    myCart.push(itme)
+    console.log(myCart)
 }
 
-console.log(myCart)
